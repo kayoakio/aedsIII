6cf1150f6b0492c85f5cee3972a94f0ff407fc15
kayoakio <42093015+kayoakio@users.noreply.github.com>
2019-06-18 16:59:02 -0300

    Add files via upload

--- Graph/graph.h
+++ Graph/graph.h
@@ -1,5 +1,3 @@
-//Grafo Não Orientado
-
 #ifndef GRAPH_H
 #define GRAPH_H
 
@@ -9,63 +7,126 @@ using namespace std;
 #include <stdlib.h>
 #include <vector>
 
-#define vertex int
+#define vertex int 
+#define branco 0
+#define cinza  1
+#define preto  2
 
 class Graph{
 
+    //Attributes
     private:
 
-        int N;
-        int M;
-        //Adjacency List
-        vector < vector<int> > adj;
+        int N; // # vertices
+        int M; // # edges
+        vector < vector < int > > adj; //Adjacency List
         
-    public: 
+        void searchdfs(vertex v, vector <vertex> &cor){
 
-    Graph(int N, int M){
+            cor[v] = cinza;
 
-        this->N = N; this->M = M;
+            for (int i = 0; i < adj[v].size(); i++){
 
-        adj.resize(N);
+                vertex w = adj[v][i];
 
-        this->adj = adj;
+                if (cor[w] == branco){
 
-    }
+                    cout << w << endl;
 
-    void addEdge(vertex v, vertex w){
+                    searchdfs(w, cor);
+                
+                }
+                
+            }
 
-        adj[v].push_back(w);
-        adj[w].push_back(v);
-
-    }
-
-    void lerGraph(){
-
-        vertex v, w;
-
-        adj.clear(); //limpa
-        adj.resize(N);
-
-        for (int i = 0; i < M; i++){
-
-            scanf("%d %d", &v, &w);
-            addEdge(v, w);
+            cor[v] = preto;
             
         }
 
-    }
+    public:
 
-    void printGraph(){
+        //Constructor
+        Graph(int N, int M){
 
-        for (int i = 0; i < N; i++){
-            cout << i << " ----> ";
-            for (int j = 0; j < adj[i].size(); j++){
-                cout << adj[i][j] << " ";
-            }
-            cout << endl;
+            this->N = N;
+            this->M = M;
+
+            adj.resize(N); //Initialization
+
+            this->adj = adj;
+
         }
 
-    }    
+        //Methods
+
+        void addEdge(vertex v, vertex w){
+
+            adj[v].push_back(w);
+            adj[w].push_back(v);
+
+        }
+
+        void readGraph(){
+
+            adj.clear();
+            adj.resize(N);
+
+            int i;
+            vertex v, w;
+
+            for (i = 0; i < M; i++){
+
+                scanf("%d %d", &v, &w);
+                addEdge(v, w);              
+
+            }        
+
+        }
+
+        void printGraph(){
+
+            int i, j;
+
+            for(i = 0; i < N; i++){
+
+                cout << i << " ----> ";
+
+                for (j = 0; j < adj[i].size(); j++){
+
+                    cout << adj[i][j] << " ";
+
+                }
+                
+                cout << endl;
+
+            }
+        }
+        
+        //DFS - Depth-First Search
+
+        void dfs (){
+
+            vector <vertex>  cor(N, 0);
+
+            for (vertex v = 0; v < N; v++){
+
+                if(cor[v] == branco){
+
+                    cout << v << endl;
+
+                    searchdfs(v, cor);
+
+                }
+
+                //cout << cor[v] << endl;
+
+            }
+
+        }
+
+        
+
+
 };
 
-#endif
+#endif //GRAPH_H
