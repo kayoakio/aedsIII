6e4b518b2cf5c33f73f2bf52973c9c507a78a55a
kayoakio <42093015+kayoakio@users.noreply.github.com>
2019-06-18 16:52:24 -0300

    Add files via upload

---
+++ Graph/graph.h
@@ -0,0 +1,73 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <iostream>
+using namespace std;
+#include <locale.h>
+#include <stdlib.h>
+#include <vector>
+
+#define vertex int
+
+class Graph{
+
+    private:
+
+        int N;
+        int M;
+        //Adjacency List
+        vector < vector<int> > adj;
+        
+    public: 
+
+    Graph(int N, int M){
+
+        this->N = N; this->M = M;
+
+        adj.resize(N);
+
+        this->adj = adj;
+
+    }
+
+    void addEdge(vertex v, vertex w){
+
+        adj[v].push_back(w);
+        adj[w].push_back(v);
+
+    }
+
+    void lerGraph(){
+
+        vertex v, w;
+
+        adj.clear(); //limpa
+        adj.resize(N);
+
+        for (int i = 0; i < M; i++){
+
+            scanf("%d %d", &v, &w);
+            addEdge(v, w);
+            
+        }
+
+    }
+
+    void printGraph(){
+
+        for (int i = 0; i < N; i++){
+            cout << i << " ----> ";
+            for (int j = 0; j < adj[i].size(); j++){
+                cout << adj[i][j] << " ";
+            }
+            cout << endl;
+        }
+
+    }
+
+    //Depth Fist Search
+    
+    
+};
+
+#endif
